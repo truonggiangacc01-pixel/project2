3629f1399f432db7eb9ae3ff360550c31a98e645
truonggiangacc01-pixel <truonggiangacc01@gmail.com>
2026-05-29 19:27:30 +0700

    đổ dữ liệu vào db sql của 1 Fk

--- src/main/java/project2/enums/TicketStatus.java
+++ src/main/java/project2/enums/TicketStatus.java
@@ -2,8 +2,8 @@ package project2.enums;
 
 public enum TicketStatus {
 
-    AVAILABLE,
-    SOLD,
-    USED,
-    CANCELLED
+    AVAILABLE, //có vé, nhưng chưa ai mua
+    SOLD,      //đã mua vé, nhưng giải đấu chưa diễn ra
+    USED,      //đã mua vé, giải đấu đã diễn ra
+    CANCELLED   //đã mua vé, khách hủy vé
 }
